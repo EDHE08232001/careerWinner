a02d9b42f34cebdb6f00d0cfc9e51a230df6e0f5
Edward He <edwardhe@Edwards-MacBook-Pro.local>
2025-07-25 13:53:18 +0800

    update

--- components/navigation-bar/navigation-bar.js
+++ components/navigation-bar/navigation-bar.js
@@ -1,10 +1,8 @@
+// careerWinner/components/navigation-bar/navigation-bar.js
 Component({
   options: {
-    multipleSlots: true // 在组件定义时的选项中启用多slot支持
+    multipleSlots: true 
   },
-  /**
-   * 组件的属性列表
-   */
   properties: {
     extClass: {
       type: String,
@@ -35,25 +33,19 @@ Component({
       value: false,
     },
     animated: {
-      // 显示隐藏的时候opacity动画效果
       type: Boolean,
       value: true
     },
     show: {
-      // 显示隐藏导航，隐藏的时候navigation-bar的高度占位还在
       type: Boolean,
       value: true,
       observer: '_showChange'
     },
-    // back为true的时候，返回的页面深度
     delta: {
       type: Number,
       value: 1
     },
   },
-  /**
-   * 组件的初始数据
-   */
   data: {
     displayStyle: ''
   },
@@ -72,16 +64,12 @@ Component({
       })
     },
   },
-  /**
-   * 组件的方法列表
-   */
   methods: {
     _showChange(show) {
       const animated = this.data.animated
       let displayStyle = ''
       if (animated) {
-        displayStyle = `opacity: ${show ? '1' : '0'
-          };transition:opacity 0.5s;`
+        displayStyle = `opacity: ${show ? '1' : '0'};transition:opacity 0.5s;`
       } else {
         displayStyle = `display: ${show ? '' : 'none'}`
       }
@@ -92,6 +80,7 @@ Component({
     back() {
       const data = this.data
       if (data.delta) {
+        console.log(`Navigating back ${data.delta} page(s)`); 
         wx.navigateBack({
           delta: data.delta
         })
